36f344ec3f72c666fbba0e8766fe94c28fb637be
moveplaten <935297380@qq.com>
2020-05-06 21:19:28 +0800

    Change to StretchDIBits

--- capture-test/capture-func.cpp
+++ capture-test/capture-func.cpp
@@ -15,24 +15,40 @@ int capture(HWND hwnd)
 
     SetStretchBltMode(hdcWindow, HALFTONE); //Mode
 
-    HDC hdcMem = CreateCompatibleDC(hdcWindow);
+//    HDC hdcMem = CreateCompatibleDC(hdcWindow);
     void * mempp = CaptureInternal(); //TODO
-    HBITMAP hBitmap = CreateBitmap(GetSystemMetrics(SM_CXSCREEN), GetSystemMetrics(SM_CYSCREEN), 1, 32, mempp);
-    free(mempp);
-    HGDIOBJ Old = SelectObject(hdcMem, hBitmap);
+//    HBITMAP hBitmap = CreateBitmap(GetSystemMetrics(SM_CXSCREEN), GetSystemMetrics(SM_CYSCREEN), 1, 32, mempp);
+//    free(mempp);
+//    HGDIOBJ Old = SelectObject(hdcMem, hBitmap);
 
-    StretchBlt(hdcWindow,
+    BITMAPINFO bi;
+
+    bi.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+    bi.bmiHeader.biWidth = GetSystemMetrics(SM_CXSCREEN);
+    bi.bmiHeader.biHeight = - GetSystemMetrics(SM_CYSCREEN);
+    bi.bmiHeader.biPlanes = 1;
+    bi.bmiHeader.biBitCount = 32;
+    bi.bmiHeader.biCompression = BI_RGB;
+    bi.bmiHeader.biSizeImage = 0;
+    bi.bmiHeader.biXPelsPerMeter = 0;
+    bi.bmiHeader.biYPelsPerMeter = 0;
+    bi.bmiHeader.biClrUsed = 0;
+    bi.bmiHeader.biClrImportant = 0;
+
+    StretchDIBits(hdcWindow,
         0, 0,
         rcClient.right, rcClient.bottom,
-        hdcMem,
         0, 0,
         GetSystemMetrics(SM_CXSCREEN),
         GetSystemMetrics(SM_CYSCREEN),
+        mempp,
+        &bi,
+        DIB_RGB_COLORS,
         SRCCOPY);
-
-    SelectObject(hdcMem, Old);
-    DeleteObject(hBitmap);
-    ReleaseDC(hwnd, hdcMem);
+    free(mempp);
+//    SelectObject(hdcMem, Old);
+//    DeleteObject(hBitmap);
+//    ReleaseDC(hwnd, hdcMem);
     ReleaseDC(hwnd, hdcWindow);
 
     return 0;
